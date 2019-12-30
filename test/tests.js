3d9fbe22a1a1e2d5e5c5d4c528c3cf999192ceaf
Glitch (toms-exam-scheduler) <none>
2019-12-30 11:14:31 -0500

    Added TODO stub for /api/courses POST.

--- test/tests.js
+++ test/tests.js
@@ -127,3 +127,74 @@ describe('/api/exams route tests', function() {
       })    
     })
   });
+
+
+  describe('/api/courses', function (){
+    describe('GET', function (){
+      it('response with JSON of courses in semester', function (done){
+        chai.request(server)
+        .get('/api/courses')
+        .query({
+          semester: 9909
+        })
+        .end( function(err, res) {
+          if(err){done(err)}
+          else{
+            assert.equal(res.status, 200);
+            assert.property(res.body[0], 'semester');
+            assert.equal(res.body[0].semester, 9909);
+            assert.property(res.body[0], 'unit');
+            assert.property(res.body[0], 'subject');
+            assert.property(res.body[0], 'course');
+            assert.property(res.body[0], 'section');
+            assert.property(res.body[0], 'index');
+            assert.property(res.body[0], 'courseTitle');
+            assert.property(res.body[0], 'assignedInstructor');
+            assert.property(res.body[0], 'day');
+            assert.property(res.body[0], 'startTime');
+            assert.property(res.body[0], 'endTime');
+            assert.property(res.body[0], 'building');
+            assert.property(res.body[0], 'room');
+            assert.property(res.body[0], 'program');
+            assert.property(res.body[0], 'level');
+            assert.property(res.body[0], 'uniqueId');
+            assert.property(res.body[0], 'examsoft');
+            assert.property(res.body[0], 'final');
+            assert.property(res.body[0], 'enrollment');
+            assert.property(res.body[0], 'sectionNickname');
+
+            done();
+          }
+        })
+      })
+    })
+
+
+    describe('POST', function(){
+      it('upload/add a set of courses', function(done){
+        chai.request(server)
+        .post('/api/courses')
+        .query({
+          semester: 9909
+        })
+        /*
+        TODO:
+        attach sample file using ".attach(field, file, attachment)",
+        e.g. .attach('imageField', fs.readFileSync('avatar.png'), 'avatar.png')
+        THEN remove courses after tests using Mocha methods.
+        */
+        .end(function (err, res){
+          if(err){
+            done(err)
+          } else {
+            //insert assertion tests here
+            assert.fail("upload file attachment test not setup yet.")
+            done();
+          }
+        })
+      })
+    })
+
+
+
+  })
