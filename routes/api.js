4eb91a8f79970f1f61d5885dcec30daae25780b4
Glitch (toms-exam-scheduler) <none>
2020-01-11 20:26:21 -0500

    Moved /api/exam routes to seperate file.

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const csv = require('csvtojson');
 mongoose.set('useFindAndModify', false);
 const loadExams = require('../test/loadExams.js');
+let sampleExams = require('../test/sampleExams.json');
+const exams = require('exams.js');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 module.exports = function (app) {
   const Schema = mongoose.Schema;
@@ -23,59 +26,7 @@ module.exports = function (app) {
   /*
   EXAM ROUTES
   */
-  
-  //exam schema and model
-  const examSchema = new Schema({
-    courseId: {type: ObjectId, required: true},
-    examName: {type: String, required: true},
-    examDate: {type: String},
-    examStart: {type: String},
-    examEnd: {type: String},
-    exanSoftware: {type: String},
-    examSemester: {type: String, required: true},
-    examDuration: {type: Number},
-    emailFaculty: {type: Boolean},
-    facultyConfirmed: {type: Boolean},
-    building: {type: String},
-    room: {type: String},
-    examNotes: {type: String},
-    supportPerson: {type: String},
-    approved: {type: Boolean}
-  })
-
-  const Exam = mongoose.model('Exam', examSchema);
-
-  app.route('/api/exam')
-
-    .post(function (req, res) {
-      const newExam = new Exam({
-        courseId: req.body.uniqueId,
-        examName: req.body.examName,
-        examDate: req.body.examDate,
-        examStart: req.body.examStart,
-        examEnd: req.body.examEnd,
-        examSoftware: req.body.examSoftware,
-        examSemester: req.body.examSemester,
-        examDuration: req.body.examDuration,
-        emailFaculty: req.body.emailFaculty,
-        facultyConfirmed: req.body.facultyConfirmed,
-        building: req.body.building,
-        room: req.body.room,
-        examNotes: req.body.examNotes,
-        supportPerson: req.body.supportPerson,
-        approved: req.body.approved
-      })
-
-      newExam.save(function (err, doc){
-        if(err){console.error(err)}
-        else{res.send(`Sucess, exam saved!`)}
-      })
-    })
-
-    .get(function (req, res) {
-
-    })
-
+  exams.examRoutes();
 
 
   /*
---
+++ routes/exams.js
@@ -0,0 +1,56 @@
+
+//exam schema and model
+const examSchema = new Schema({
+    courseId: {type: ObjectId, required: true},
+    examName: {type: String, required: true},
+    examDate: {type: String},
+    examStart: {type: String},
+    examEnd: {type: String},
+    exanSoftware: {type: String},
+    examSemester: {type: String, required: true},
+    examDuration: {type: Number},
+    emailFaculty: {type: Boolean},
+    facultyConfirmed: {type: Boolean},
+    building: {type: String},
+    room: {type: String},
+    examNotes: {type: String},
+    supportPerson: {type: String},
+    approved: {type: Boolean}
+})
+
+const Exam = mongoose.model('Exam', examSchema);
+function examRoutes () {
+    app.route('/api/exam')
+    
+    .post(function (req, res) {
+        const newExam = new Exam({
+            courseId: req.body.uniqueId,
+            examName: req.body.examName,
+            examDate: req.body.examDate,
+            examStart: req.body.examStart,
+            examEnd: req.body.examEnd,
+            examSoftware: req.body.examSoftware,
+            examSemester: req.body.examSemester,
+            examDuration: req.body.examDuration,
+            emailFaculty: req.body.emailFaculty,
+            facultyConfirmed: req.body.facultyConfirmed,
+            building: req.body.building,
+            room: req.body.room,
+            examNotes: req.body.examNotes,
+            supportPerson: req.body.supportPerson,
+            approved: req.body.approved
+        })
+        
+        newExam.save(function (err, doc){
+            if(err){console.error(err)}
+            else{res.send(`Sucess, exam saved!`)}
+        })
+    })
+    
+    .get(function (req, res) {
+        res.send(`/api/exam GET route worked!`)
+    })
+}
+exports.examSchema = examSchema;
+exports.Exam = Exam;
+exports.examRoutes = examRoutes;
