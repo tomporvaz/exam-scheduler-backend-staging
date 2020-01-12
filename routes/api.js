9792ac5573ab548399f5312967485e7c1bc13ac6
Glitch (toms-exam-scheduler) <none>
2020-01-11 19:10:59 -0500

    Moved sample files to test folder, and wrote new module to handle saving sample exams to DB.

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const csv = require('csvtojson');
 mongoose.set('useFindAndModify', false);
+const loadExams = require('../test/loadExams.js');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -15,8 +16,73 @@ module.exports = function (app) {
   db.once('open', function (){
     console.log("DB sucess using mongoose!")
   });
+
+  loadExams('../test/sampleExams.json')
   
-  //schema and model
+
+  /*
+  EXAM ROUTES
+  */
+  
+  //exam schema and model
+  const examSchema = new Schema({
+    courseId: {type: ObjectId, required: true},
+    examName: {type: String, required: true},
+    examDate: {type: String},
+    examStart: {type: String},
+    examEnd: {type: String},
+    exanSoftware: {type: String},
+    examSemester: {type: String, required: true},
+    examDuration: {type: Number},
+    emailFaculty: {type: Boolean},
+    facultyConfirmed: {type: Boolean},
+    building: {type: String},
+    room: {type: String},
+    examNotes: {type: String},
+    supportPerson: {type: String},
+    approved: {type: Boolean}
+  })
+
+  const Exam = mongoose.model('Exam', examSchema);
+
+  app.route('/api/exam')
+
+    .post(function (req, res) {
+      const newExam = new Exam({
+        courseId: req.body.uniqueId,
+        examName: req.body.examName,
+        examDate: req.body.examDate,
+        examStart: req.body.examStart,
+        examEnd: req.body.examEnd,
+        examSoftware: req.body.examSoftware,
+        examSemester: req.body.examSemester,
+        examDuration: req.body.examDuration,
+        emailFaculty: req.body.emailFaculty,
+        facultyConfirmed: req.body.facultyConfirmed,
+        building: req.body.building,
+        room: req.body.room,
+        examNotes: req.body.examNotes,
+        supportPerson: req.body.supportPerson,
+        approved: req.body.approved
+      })
+
+      newExam.save(function (err, doc){
+        if(err){console.error(err)}
+        else{res.send(`Sucess, exam saved!`)}
+      })
+    })
+
+    .get(function (req, res) {
+
+    })
+
+
+
+  /*
+  COURSE ROUTES
+  */
+
+  //course schema and model
   const courseSchema = new Schema({
     uniqueId: {type: Number, required: true},
     semester: {type: String, required: true},
---
+++ test/loadExams.js
@@ -0,0 +1,30 @@
+module.exports = function loadExams (examJson) {
+    
+    examJson.forEach((examData) => {
+        const newExam = new Exam({
+            courseId: examData.courseId,
+            examName: examData.examName,
+            examDate: examData.examDate,
+            examStart: examData.examStart,
+            examEnd: examData.examEnd,
+            examSoftware: examData.examSoftware,
+            examSemester: examData.examSemester,
+            examDuration: examData.examDuration,
+            emailFaculty: examData.emailFaculty,
+            facultyConfirmed: examData.facultyConfirmed,
+            building: examData.building,
+            room: examData.room,
+            examNotes: examData.examNotes,
+            supportPerson: examData.supportPerson,
+            approved: examData.approved
+        })
+        
+        newExam.save(function (err, doc){
+            if(err){console.error(err)}
+            else{console.log(`Sucess, ${newExam.examName} saved!`)}
+        }) 
+    })
+    
+    
+    
+}
