d0c4f46a22b62d637828dfd4aee10d6a25703dda
Glitch (toms-exam-scheduler) <none>
2020-03-08 21:12:11 -0400

    Added filters.js to collect filters from exam list and deliver on a GET route.

---
+++ routes/fitlers.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+function filters (app) {
+    app.route('/api/filters')
+    
+    .post(function (req, res) {
+      //the post route should update a persistent record of which filterGroups should be included in query and GET response
+      res.send("This route is not configured yet.  Filter query params are hard coded in filters route.")
+      })
+      
+      .get(function (req, res) {
+        Exam.find({examSemester: req.query.semester})
+        .populate('courseId')
+        .exec(
+          function(err, doc){
+            if(err){console.error(err)}
+            else{
+              //flatten course data forEach exam into itself and update _id to examId
+              let arrFlatExams = doc.map((exam) => {
+                let newExam = {...exam._doc};
+                
+                //change exam._id to examId
+                newExam.examId = newExam._id;
+                delete newExam._id;
+                
+                //spread newExam and courseId to flatten course
+                newExam = {...newExam, ...newExam.courseId._doc}
+                
+                //change courseId._id to courseId
+                newExam.courseId = newExam.courseId._id;
+                
+                delete newExam._id;
+                delete newExam.__v
+                
+                return newExam;
+              })
+              res.json(arrFlatExams);   
+            }
+          })
+        })
+        
+        .put(function(req, res){
+          
+          let updateObj = {};
+          
+          
+          if(req.body.courseId){updateObj.courseId = req.body.courseId};
+          if(req.body.examDate){updateObj.examDate = req.body.examDate};
+          if(req.body.examName){updateObj.examName = req.body.examName};
+          if(req.body.examStart){updateObj.examStart = req.body.examStart};
+          if(req.body.examEnd){updateObj.examEnd = req.body.examEnd};
+          if(req.body.examSoftware){updateObj.examSoftware = req.body.examSoftware};
+          if(req.body.examSemester){updateObj.examSemester = req.body.examSemester};
+          if(req.body.examDuration){updateObj.examDuration = req.body.examDuration};
+          if(req.body.emailFaculty){updateObj.emailFaculty = req.body.emailFaculty};
+          if(req.body.facultyConfirmed){updateObj.facultyConfirmed = req.body.facultyConfirmed};
+          if(req.body.building){updateObj.building = req.body.building};
+          if(req.body.room){updateObj.room = req.body.room};
+          if(req.body.examNotes){updateObj.examNotes = req.body.examNotes};
+          if(req.body.supportPerson){updateObj.supportPerson = req.body.supportPerson};
+          if(req.body.approved){updateObj.approved = req.body.approved};
+          
+          Exam.findByIdAndUpdate(req.query.examId, updateObj,{new: true}, 
+            function(err, doc){
+              if(err){console.error(err)}
+              else{
+                //convert _id to examId
+                doc._doc.examId = doc._doc._id;
+                delete doc._doc._id;
+  
+                //send new doc
+                res.json(doc);
+              }
+            } )
+            
+          })
+        }
+        exports.filters = filters;
+        
