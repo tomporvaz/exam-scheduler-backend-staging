c65baa20f01c90fd215148d91c276445fbf24d72
Glitch (toms-exam-scheduler) <none>
2020-07-29 20:00:32 -0400

    Convert date-times to readable format.

--- routes/export-exams.js
+++ routes/export-exams.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const { Exam, arrFlattenExam } = require('./exams');
+const moment = require('moment');
 
 
 function exportExams(app){
-  app.get('/api/exportExams', function(req, res){
+  app.get('/api/export-exams', function(req, res){
     Exam.find({examSemester: req.query.semester})
     .populate('courseId')
     .sort({'examStart': 'ascending'})
@@ -11,8 +12,16 @@ function exportExams(app){
       function(err, doc){
         if(err){console.error(err)}
         else{
+          let flattenedExams = doc.map(exam => arrFlattenExam(exam));
+          let examsWithReadableTimes = flattenedExams.map(exam => {
+            let newExam = {...exam};
+            newExam.startTime = moment(exam.examStart).format("dddd, MMMM Do YYYY, h:mm:ss a");
+            newExam.endTime = moment(exam.examEnd).format("dddd, MMMM Do YYYY, h:mm:ss a");
+            
+            return(newEaxm);
+          })
           
-          res.json(doc.map(exam => arrFlattenExam(exam)));   
+          res.json(examsWithReadableTimes);   
         }
       })
     })
